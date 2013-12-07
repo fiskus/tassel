2a467f35a4b6ffa72cea007fafa55b0aaca1a15b
Maxim Chervonny <fiskus@chervonny.ru>
2013-12-08 00:58:50 +0400

    use grunt builder

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        coffee: {
+            options: {
+                bare: true
+            },
+            build: {
+                src: 'static/coffee/*.coffee',
+                dest: 'static/js/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            build: {
+                src: 'static/js/<%= pkg.name %>.js',
+                dest: 'static/js/<%= pkg.name %>.min.js'
+            }
+        },
+        lodash: {
+            options: {
+                modifier: 'modern'
+            },
+            build: {
+                dest: 'static/lib/lodash.js'
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'static/css/bookmarks.css': 'static/sass/bookmarks.sass'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-lodash');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['lodash', 'coffee', 'uglify', 'sass']);
+};
