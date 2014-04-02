4d13577def83aa96ed4cd4f0e4901761f3ed2a55
Maxim Chervonny <fiskus@chervonny.ru>
2014-04-03 00:46:29 +0400

    beautify watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,15 +30,25 @@ module.exports = function(grunt) {
             }
         },
         watch: {
-            files: ['<%= coffee.build.src %>', 'static/js/<%= pkg.name %>.js', 'stylus/*.styl'],
-            tasks: ['coffee', 'uglify', 'stylus']
+            css: {
+                files: 'stylus/*.styl',
+                tasks: 'stylus'
+            },
+            libs: {
+                files: ['static/libs/*.js'],
+                tasks: ['uglify:libs']
+            },
+            coffee: {
+                files: ['<%= coffee.build.src %>'],
+                tasks: ['coffee', 'uglify:tassel']
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-coffee');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['coffee', 'stylus', 'watch']);
 };
